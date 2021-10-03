08511bf7db4e879b69930d5d60e15a036060078b
Luciano Belo <luciano.alcantara@ufv.br>
2021-10-03 10:47:08 -0300

    add some adjusts

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,64 @@
+import { User } from ".prisma/client";
 import { Router } from "express";
-import UsersController from "../controllers/UsersController";
+import prisma from "../database";
 
 const routes = Router();
-const usersController = new UsersController();
 
-routes.get("/", usersController.get);
-routes.post("/", (usersController.create));
-routes.delete("/", (usersController.delete));
-routes.put("/", (usersController.update));
+routes.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  return res.json(users);
+});
+
+routes.post("/", async (req, res) => {
+  const { email, name, country, age } = req.body;
+
+  try {
+    const user: User = await prisma.user.create({
+      data: {
+        email: email,
+        name: name,
+        country: country,
+        age: age,
+      },
+    });
+
+    return res.json(user);
+  } catch (error) {
+    return res.json(error);
+  }
+});
+routes.delete("/", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const deletedUser: User = await prisma.user.delete({
+      where: {
+        email: email,
+      },
+    });
+
+    return res.json(deletedUser);
+  } catch (error) {
+    return res.json(error);
+  }
+});
+routes.put("/:id", async (req, res) => {
+  const { email, name, country, age } = req.body;
+
+  const updatedUser: User = await prisma.user.update({
+    where: {
+      id: Number(req.params.id),
+    },
+    data: {
+      email: email,
+      name: name,
+      country: country,
+      age: age,
+    },
+  });
+
+  return res.json(updatedUser);
+});
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
@@ -12,7 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
 
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   if (err instanceof AppError) {
@@ -30,6 +31,6 @@ app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3334, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Back-end started on port 3333! 🔥️");
 });
